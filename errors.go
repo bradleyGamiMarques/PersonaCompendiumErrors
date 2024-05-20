4363965621c31020b3bd37d2b4f6df65739e941c
bradleyGamiMarques <bradleygamimarques@outlook.com>
2024-05-19 22:25:54 -0700

    [feature] Add helper methods
    - Add UnauthorizedError helper method
    - Add ForbiddenError helper method
    - Add NotFoundError helper method
    - Add InternalServerError helper method
    - Add ToJSON method to marshal ErrorResponse
    - Add JSONResponse method to handle marshal errors
    and return APIGatewayProxyResponse with custom JSON body.

--- errors.go
+++ errors.go
@@ -1,7 +1,10 @@
 package personacompendiumerrors
 
 import (
+	"encoding/json"
 	"time"
+
+	"github.com/aws/aws-lambda-go/events"
 )
 
 type ErrorResponse struct {
@@ -20,4 +23,58 @@ func CreateErrorResponse(err string, message string, path string, statusCode int
 		StatusCode: statusCode,
 		Timestamp:  time.Now().UTC().Format(time.RFC3339),
 	}
+}
+
+func BadRequestError(message string, path string) ErrorResponse {
+	return CreateErrorResponse("Bad Request", message, path, 400)
+}
+
+func UnauthorizedError(message string, path string) ErrorResponse {
+	return CreateErrorResponse("Unauthorized", message, path, 401)
+}
+
+func ForbiddenError(message string, path string) ErrorResponse {
+	return CreateErrorResponse("Forbidden", message, path, 403)
+}
+
+func NotFoundError(message string, path string) ErrorResponse {
+	return CreateErrorResponse("Not Found", message, path, 404)
+}
+
+func InternalServerError(message string, path string) ErrorResponse {
+	return CreateErrorResponse("Internal Server Error", message, path, 500)
+}
+
+func (errResp ErrorResponse) ToJSON() (string, error) {
+	jsonBytes, err := json.Marshal(errResp)
+	if err != nil {
+		return "", nil
+	}
+	return string(jsonBytes), nil
+}
+
+func JSONResponse(errResp ErrorResponse) (events.APIGatewayProxyResponse, error) {
+	errorResponseJSON, err := errResp.ToJSON()
+	if err != nil {
+		// Return a generic error response if marshaling fails
+		genericError := ErrorResponse{
+			Error:      "Internal Server Error",
+			Message:    "An error occurred while processing the error response",
+			Path:       errResp.Path,
+			StatusCode: 500,
+			Timestamp:  time.Now().Format((time.RFC3339)),
+		}
+		genericErrorJSON, _ := json.Marshal(genericError)
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Headers:    map[string]string{"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+			Body:       string(genericErrorJSON),
+		}, nil
+	}
+	return events.APIGatewayProxyResponse{
+		StatusCode:      errResp.StatusCode,
+		Headers:         map[string]string{"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+		Body:            errorResponseJSON,
+		IsBase64Encoded: false,
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bradleyGamiMarques/PersonaCompendiumErrors
 
 go 1.21.8
+
+require github.com/aws/aws-lambda-go v1.47.0 // indirect
